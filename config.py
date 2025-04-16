87f26baab51e4ee769e6ec48c3c34648345328fe
Shreya Venkat <shreyavenkat26@gmail.com>
2025-04-15 21:01:38 -0400

    Fixed config file import error

---
+++ config.py
@@ -0,0 +1,21 @@
+FINNHUB_API_KEY = "cvs3atpr01qvc2muh4t0cvs3atpr01qvc2muh4tg"
+
+DEEPSEEK_API_KEY = "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"
+
+FINNHUB_TICKERS = [
+    "AAPL",
+    "MSFT",
+    "TSLA",
+    "AMZN",
+    "GOOG"
+]
+
+EMBEDDING_MODEL = "BAAI/bge-base-en-v1.5"
+
+QDRANT = {
+    "host": "localhost",
+    "port": 6333,
+    "collection_name": "financial_news"
+}
+
+SCHEDULE_HOURS = 3
